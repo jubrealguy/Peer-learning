181a08e9573ba0a317196ab1851f69b022e2f455
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2022-11-17 15:13:07 +0000

    two double digits

---
+++ C_projects/02-variables_condition_loops/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = a + 1; b < 100; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((a % 10) + '0');
+			
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
