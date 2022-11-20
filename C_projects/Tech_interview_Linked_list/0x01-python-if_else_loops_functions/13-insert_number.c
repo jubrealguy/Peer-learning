06fa8920419e0e31a8274f9f5e7e61b417caf0cf
Kenza1525 <kerianenzabampema@gmail.com>
2022-11-20 11:30:57 +0200

    Modified 13-isert_number.c

--- C_projects/Tech_interview_Linked_list/0x01-python-if_else_loops_functions/13-insert_number.c
+++ C_projects/Tech_interview_Linked_list/0x01-python-if_else_loops_functions/13-insert_number.c
@@ -6,10 +6,6 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *temp = NULL;
 	listint_t *new = NULL;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
@@ -26,14 +22,29 @@ listint_t *insert_node(listint_t **head, int number)
 
 	temp = *head;
 
-	while (temp != NULL)
+	while (temp->next != NULL)
 	{
-		if (temp->n < new->n && temp->next->n > new->n)
+		/*if (temp->n > new->n)
+                {
+                        new->next = temp;
+                        *head = new;
+                        return (new);
+                } This is the part messing with our code*/
+
+		else if (temp->n < new->n && temp->next->n > new->n)
 		{
 			new->next = temp->next;
 			temp->next = new;
 		}
+
 		temp = temp->next;
+
+	}
+
+	if (temp->n < new->n)
+	{
+		new->next = NULL;
+		temp->next = new;
 	}
 	return (new);
 	
