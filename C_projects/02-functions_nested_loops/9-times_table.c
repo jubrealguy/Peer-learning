d983196249ce6d427a2d3348b7c8a6fe321c8f99
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2022-12-06 08:08:19 +0000

    renamed folders

--- C_projects/02-functions_nested_loops/9-times_table.c
+++ C_projects/02-functions_nested_loops/9-times_table.c
@@ -9,8 +9,11 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
+			
+			if (j == 0)
+				_putchar(k + '0');
 
-			if (k < 10)
+			else if (k < 10)
 			{
 				_putchar(' ');
 				_putchar(k + '0');
