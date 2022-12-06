707447cb42c2a28aba9064f13221610c4fb2885c
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2022-12-06 08:36:02 +0000

    print 0 -14

---
+++ C_projects/04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num < 15; num ++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
