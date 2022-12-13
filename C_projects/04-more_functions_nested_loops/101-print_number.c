10c909b6c45de0df834a6822e53e9de9dc4300be
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2022-12-13 00:00:18 +0000

    solved simpler

--- C_projects/04-more_functions_nested_loops/101-print_number.c
+++ C_projects/04-more_functions_nested_loops/101-print_number.c
@@ -2,34 +2,25 @@
 
 void print_number(int n)
 {
-        int times_ten = 1;
+        int times_ten = 10;
         int num;
 
-        if (n == 0)
-        {
-                _putchar('0');
-        }
-        else
-        {
-                if (n < 0)
-                {
-                        n *= -1;
-                        _putchar('-');
-                }
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	while (times_ten < n)
+	{
+		times_ten *= 10;
+	}
+	times_ten /= 10;
 
-                while (times_ten < n)
-                {
-                        times_ten *=  10;
-                }
-                times_ten /= 10;
-
-                while (times_ten >= 1)
-                {
-                        num = n / times_ten;
-                        _putchar(num + '0');
-                        n = n - (num * times_ten);
-                        times_ten /= 10;
-                }
-
-        }
+	while (times_ten > 0)
+	{
+		num = n / times_ten;
+		_putchar(num + '0');
+		n = n - (num * times_ten);
+		times_ten /= 10;
+	}
 }
