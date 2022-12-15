473dbe91fafc2928f49814f9a869a48571f29585
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2022-12-15 19:27:12 +0000

    uncompleted task 5

---
+++ C_projects/05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+	int i, j = 0;
+	int tmp;
+
+	for (i = 0; s[i]; i++)
+		;
+
+	i--;
+
+	for (; j > i; i--)
+	{
+		tmp = s[j]; 
+		s[j] = s[i];
+		s[j] = tmp;
+		j++;
+	}
+}
