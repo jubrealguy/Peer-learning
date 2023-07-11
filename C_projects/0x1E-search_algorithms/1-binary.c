70491b97a51eb327f24198639eda3847351bacc5
jubrealguy <whizkidjubrealguy@gmail.com>
2023-07-11 21:15:54 +0100

    binary

---
+++ C_projects/0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+int binary_search(int *array, size_t size, int value)
+{
+	int i, size = (int)size;
+	int low = 0, high = end, mid = ;
+	
+	for (i = 0; i < siz; i++)
+	{
+		
+	}
+}
