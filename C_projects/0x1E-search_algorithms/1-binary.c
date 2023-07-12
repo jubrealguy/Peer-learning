f2ef43b30dfd04de347bf9ed11993fd23dee7b12
jubrealguy <whizkidjubrealguy@gmail.com>
2023-07-12 22:58:12 +0100

    binary dearch

--- C_projects/0x1E-search_algorithms/1-binary.c
+++ C_projects/0x1E-search_algorithms/1-binary.c
@@ -3,11 +3,26 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int i, size = (int)size;
-	int low = 0, high = end, mid = ;
+	int siz = (int)size;
+	int low = 0;
+	int high = siz-1;
 	
-	for (i = 0; i < siz; i++)
+	while (low <= high)
 	{
-		
+		int mid = low + (high - low)/2;
+
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		else if (array[mid] > value)
+		{
+			high = mid - 1;
+		}
 	}
+	return (-1);
 }
